c184d749c6c05013e3ff8858988b65a0c7a0b152
OOOHA <45551120+OOOHA@users.noreply.github.com>
2021-08-06 15:55:40 +0800

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by 邱宏宇 on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
