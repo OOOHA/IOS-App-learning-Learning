bf8ef804cce6904bb40c0dd4a2fb9cc8d52e2905
OOOHA <45551120+OOOHA@users.noreply.github.com>
2021-12-29 22:52:04 +0800

    first

--- Learning/ContentView.swift
+++ Learning/ContentView.swift
@@ -7,15 +7,113 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    
+    
+    
+    
+    
+    
+    @State var word = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ZStack{
+                Color("first")
+                    .ignoresSafeArea()
+                
+                VStack{
+                    ZStack(alignment: .trailing){
+
+                        TextField("Input", text: $word)
+                            .frame(width: 300, height: 50)
+                            .background(Color.white)
+                            .cornerRadius(30)
+                            .padding()
+                            
+//                        Button(action: {
+//                            word = ""
+//                        }) {
+//                            Image(systemName: "xmark.circle.fill")
+//                                .padding(.trailing)
+//                                .foregroundColor(.red)
+//                                .offset(x:-5)
+//                                //.cornerRadius(50)
+//                        }
+                    }
+                    
+                    
+                    NavigationLink(
+                        destination: sec(new: word),
+                        label: {
+                            Text("Hello, world!")
+                                .frame(width: 300, height: 50)
+                                .background(Color.green)
+                                .cornerRadius(30)
+                                .padding()
+                                .foregroundColor(Color("on"))
+                                .padding()
+                        })
+                    
+                    NavigationLink(
+                        destination: sec(new: word),
+                        label: {
+                            Text("Hi")
+                                .frame(width: 300, height: 50)
+                                .background(Color.gray)
+                                .cornerRadius(30)
+                                .padding()
+                                .foregroundColor(Color("on"))
+                                .padding()
+                        })
+                    
+                    
+                    NavigationLink(
+                        destination: sec(new: word),
+                        label: {
+                            Button(action: {
+                                word = "100"
+                            }, label: {
+                                Text("Button")
+                                    .frame(width: 300, height: 50)
+                                    .background(Color.blue)
+                                    .cornerRadius(30)
+                                    .padding()
+                                    .foregroundColor(Color("on"))
+                                    .padding()
+                                    .padding()
+                                
+                            })
+                        })
+                    
+                }
+                
+                
+            }
+            
+        }
+    }
+}
+struct sec: View {
+    
+    @State var new = ""
+    var body: some View {
+        VStack{
+            Text(new)
+                .padding()
+            Text("Fuck")
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
